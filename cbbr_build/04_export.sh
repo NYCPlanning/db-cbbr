6fa8bc77501be24d96bebf0f19bf30e39a816497
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-11-08 17:16:42 -0500

    changing path to credentials

--- cbbr_build/04_export.sh
+++ cbbr_build/04_export.sh
@@ -5,8 +5,8 @@ REPOLOC="$(git rev-parse --show-toplevel)"
 cd $REPOLOC
 
 # load config
-DBNAME=$(cat $REPOLOC/cpdb.config.json | jq -r '.DBNAME')
-DBUSER=$(cat $REPOLOC/cpdb.config.json | jq -r '.DBUSER')
+DBNAME=$(cat $REPOLOC/cbbr_build/cbbr.config.json | jq -r '.DBNAME')
+DBUSER=$(cat $REPOLOC/cbbr_build/cbbr.config.json | jq -r '.DBUSER')
 
 # export csv
 psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions) TO '$REPOLOC/cbbr_build/output/cbbr_submissions.csv' DELIMITER ',' CSV HEADER;"

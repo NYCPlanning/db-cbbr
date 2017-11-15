be018ea8ba7f1457d57939972b2f29b4bde6f038
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-11-14 19:03:47 -0500

    limiting export to completed CBs

--- cbbr_build/04_export.sh
+++ cbbr_build/04_export.sh
@@ -9,13 +9,13 @@ DBNAME=$(cat $REPOLOC/cbbr.config.json | jq -r '.DBNAME')
 DBUSER=$(cat $REPOLOC/cbbr.config.json | jq -r '.DBUSER')
 
 # export csv
-psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions) TO '$REPOLOC/cbbr_build/output/cbbr_submissions.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions WHERE borough IS NOT NULL) TO '$REPOLOC/cbbr_build/output/cbbr_submissions.csv' DELIMITER ',' CSV HEADER;"
 
 # points
-pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_pts $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPoint'"
+pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_pts $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPoint' AND borough IS NOT NULL"
 
 # polygons
-pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_poly $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPolygon'"
+pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_poly $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPolygon' AND borough IS NOT NULL"
 
 # for manual geom creation
 psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions WHERE geom IS NULL AND borough IS NOT NULL AND budgetcategory = 'Capital' AND site1 LIKE 'Yes%') TO '$REPOLOC/cbbr_build/output/cbbr_submissions_needgeoms.csv' DELIMITER ',' CSV HEADER;"

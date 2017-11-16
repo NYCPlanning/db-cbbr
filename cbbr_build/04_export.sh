eafd25ed923fd1f36378d168c5744cff616eff2c
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-11-16 16:50:19 -0500

    exporting geojson

--- cbbr_build/04_export.sh
+++ cbbr_build/04_export.sh
@@ -12,10 +12,13 @@ DBUSER=$(cat $REPOLOC/cbbr.config.json | jq -r '.DBUSER')
 psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions WHERE borough IS NOT NULL) TO '$REPOLOC/cbbr_build/output/cbbr_submissions.csv' DELIMITER ',' CSV HEADER;"
 
 # points
-pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_pts $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPoint' AND borough IS NOT NULL"
+ogr2ogr -f "GeoJSON" cbbr_build/output/cbbr_submissions_pts.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" \
+-sql "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPoint' AND borough IS NOT NULL"
 
 # polygons
-pgsql2shp -u $DBUSER -f cbbr_build/output/cbbr_submissions_poly $DBNAME "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPolygon' AND borough IS NOT NULL"
+ogr2ogr -f "GeoJSON" cbbr_build/output/cbbr_submissions_poly.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" \
+-sql "SELECT * FROM cbbr_submissions WHERE ST_GeometryType(geom)='ST_MultiPolygon' AND borough IS NOT NULL"
 
 # for manual geom creation
 psql -U $DBUSER -d $DBNAME -c "COPY (SELECT * FROM cbbr_submissions WHERE geom IS NULL AND borough IS NOT NULL AND budgetcategory = 'Capital' AND site1 LIKE 'Yes%') TO '$REPOLOC/cbbr_build/output/cbbr_submissions_needgeoms.csv' DELIMITER ',' CSV HEADER;"
+

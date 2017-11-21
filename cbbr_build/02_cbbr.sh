18013e231905e5f7ea40b76dac0d907d207758bf
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-11-21 17:45:48 -0500

    process for changing the site type

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -16,4 +16,4 @@ echo 'Normalizing data...'
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/cbbr_build/sql/normalize_agency.sql
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/cbbr_build/sql/normalize_agencyacro.sql
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/cbbr_build/sql/normalize_denominator.sql
-
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/cbbr_build/sql/normalize_sitetype.sql

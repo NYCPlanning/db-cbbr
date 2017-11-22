0625f8f01d680c1f51ef08f62cc9a31a5bdec1a8
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-11-22 15:56:21 -0500

    writing readme and expanding what datasets are needed and loaded

--- cbbr_build/01_dataloading.sh
+++ cbbr_build/01_dataloading.sh
@@ -14,3 +14,15 @@ cd '/prod/data-loading-scripts'
 echo 'Loading core datasets'
 node loader.js install cbbr_submissions
 node loader.js install cbbr_geoms
+node loader.js install dpr_parksproperties
+
+cd '/prod/db-cbbr'
+
+DBNAME=$(cat $REPOLOC/cbbr.config.json | jq -r '.DBNAME')
+DBUSER=$(cat $REPOLOC/cbbr.config.json | jq -r '.DBUSER')
+
+# facilties from carto
+wget -O './capitalprojects_build/downloads/facdb_facilities.json' 'https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?format=GeoJSON&q=select * from facdb_facilities'
+
+ogr2ogr -f 'PostgreSQL' PG:'dbname=$DBNAME user=$DBUSER' $REPOLOC/'capitalprojects_build/downloads/facdb_facilities.json' -nln facdb_facilities -overwrite
+rm './capitalprojects_build/downloads/facdb_facilities.json'

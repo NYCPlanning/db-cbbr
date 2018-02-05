1ff701f10539fad4f8bfa355cd756c356083f16f
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-02-05 18:27:13 -0500

    adding logic for summary stats

--- analysis/sql/agency_cd_summarystats.sql
+++ analysis/sql/agency_cd_summarystats.sql
@@ -1,17 +1,19 @@
-CREATE EXTENSION tablefunc;
+SELECT agencyacro, COUNT(*)
+FROM cbbr_submissions
+GROUP BY agencyacro
+ORDER BY count DESC;
 
-SELECT *
-FROM   crosstab(
-   'SELECT agency, commdist, COUNT(*)
-    FROM   cbbr_submissions
-    GROUP BY agency, commdist
-    ORDER  BY 1,2'  -- needs to be "ORDER BY 1,2" here
-   ) AS ct (agency text, commdist int);
+SELECT commdist, COUNT(*)
+FROM cbbr_submissions
+GROUP BY commdist
+ORDER BY count DESC;
 
+SELECT name, COUNT(*)
+FROM cbbr_submissions
+GROUP BY name
+ORDER BY count DESC;
 
-SELECT *
-FROM crosstab(
-  'SELECT agency, commdist, COUNT(*)
-   FROM cbbr_submissions
-   GROUP BY agency, commdist')
-AS ct(row_name text, category_1 text, category_2 text, category_3 text);
+SELECT borough, COUNT(*)
+FROM cbbr_submissions
+GROUP BY borough
+ORDER BY count DESC;

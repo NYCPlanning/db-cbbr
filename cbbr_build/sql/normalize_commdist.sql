432e068bd2203be28201d0bcb6a8fb9863946d98
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-02-12 11:56:14 -0500

    adding logic to not overwrite comm dist when it's already 3 characters long

--- cbbr_build/sql/normalize_commdist.sql
+++ cbbr_build/sql/normalize_commdist.sql
@@ -1,19 +1,24 @@
 UPDATE cbbr_submissions a
 SET commdist = '1'||lpad(commdist, 2, '0')
-WHERE a.borough='Manhattan';
+WHERE a.borough='Manhattan'
+AND length(commdist)>3;
 
 UPDATE cbbr_submissions a
 SET commdist = '2'||lpad(commdist, 2, '0')
-WHERE a.borough='Bronx';
+WHERE a.borough='Bronx'
+AND length(commdist)>3;
 
 UPDATE cbbr_submissions a
 SET commdist = '3'||lpad(commdist, 2, '0')
-WHERE a.borough='Brooklyn';
+WHERE a.borough='Brooklyn'
+AND length(commdist)>3;
 
 UPDATE cbbr_submissions a
 SET commdist = '4'||lpad(commdist, 2, '0')
-WHERE a.borough='Queens';
+WHERE a.borough='Queens'
+AND length(commdist)>3;
 
 UPDATE cbbr_submissions a
 SET commdist = '5'||lpad(commdist, 2, '0')
-WHERE a.borough='Staten Island';
+WHERE a.borough='Staten Island'
+AND length(commdist)>3;

3fc85d76973ac6fcebf9f7ba94f7878f98e7a63c
ZhouBaoling <zhoubaoling@ZhouBaolingsAir.home>
2019-12-16 21:19:18 -0500

    update aggregate_geoms.py

--- cbbr_build/01_dataloading.sh
+++ cbbr_build/01_dataloading.sh
@@ -31,16 +31,9 @@ docker run --rm\
 
 docker run --rm\
             --network=host\
-            -v `pwd`/python:/home/python\
-            -w /home/python\
-            --env-file .env\
-            sptkl/cook:latest bash -c "pip3 install -r requirements.txt; python3 facdb_dataloading.py"
-
-docker run --rm\
-            --network=host\
             -v `pwd`:/home/cbbr_build\
             -w /home/cbbr_build\
             --env-file .env\
-            sptkl/cook:latest bash -c "pip3 install -r python/requirements.txt; python3 python/aggregate_geoms.py"
+            sptkl/cook:latest bash -c "pip3 install -r python/requirements.txt; python3 python/facdb_dataloading.py; python3 python/aggregate_geoms.py"
 
 docker exec $DB_CONTAINER_NAME psql -U postgres -h localhost -f sql/preprocessing.sql
--- cbbr_build/python/aggregate_geoms.py
+++ cbbr_build/python/aggregate_geoms.py
@@ -13,11 +13,25 @@ if __name__ == '__main__':
     for f in json_data:
         with open(f,encoding='utf-8', errors='ignore') as json_data:
             info = json.load(json_data, strict=False)
+        count = str(info['coordinates']).count(',')
+        type = info['type']
+        info = str(info).replace("'",'"')
+        if (type == 'LineString')&(count == 3):
+            info = info.replace("[[[",'[[')
+            info = info.replace("]]]",']]')
+        if (type == 'LineString')&(count > 3)&('[[[' in info):
+            info = info.replace("LineString",'Polygon')
+            type = 'Polygon'
+        if (type == 'LineString')&(count > 3)&('[[[' not in info):
+            info = info.replace("LineString",'Polygon')
+            type = 'Polygon'
+            info = info.replace("[[",'[[[')
+            info = info.replace("]]",']]]')
         name = str(f)[str(f).find('geometries/')+len('geometries/'):-5]
         result = dict(
             regid = name,
-            type = info['type'], 
-            geom = info['coordinates']
+            type = type,
+            geom = info
         )
         data.append(result)
 

de9f7dcaaf368de73c830135965212e4358638b2
Baoling Zhou <baolingz@nyu.edu>
2019-12-17 19:47:36 +0000

    incorporate dockerize-baiyue branch

--- cbbr_build/python/dataloading.py
+++ cbbr_build/python/dataloading.py
@@ -10,8 +10,13 @@ def ETL():
     importer.import_table(schema_name='cbbr_submissions')
     importer.import_table(schema_name='dpr_parksproperties')
 
+def FACDB():
+    EDM_DATA = os.environ.get('EDM_DATA', '')
+    BUILD_ENGINE=os.environ.get('BUILD_ENGINE', '')
+
+    importer = Importer(EDM_DATA, BUILD_ENGINE)
+    importer.import_table(schema_name='facilities')
+
 if __name__ == "__main__":
-    beg_ts = time.time()
     ETL()
-    end_ts = time.time()
-    print(f'loading time: {(end_ts - beg_ts)/60:.3f} minutes')
+    FACDB()
--- cbbr_build/sql/preprocessing.sql
+++ cbbr_build/sql/preprocessing.sql
@@ -1,6 +1,3 @@
-ALTER TABLE facdb_facilities
-ALTER COLUMN geom TYPE geometry(POINT, 4326) USING ST_GeomFromText(ST_AsText(geom),4326);
-
 UPDATE cbbr_geoms
 SET geom = ST_SetSRID(ST_GeomFromGeoJSON(geom),4326);
 

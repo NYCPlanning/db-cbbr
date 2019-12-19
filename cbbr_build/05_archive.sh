0021bc482ec4100e96453217b30a4ada73a9d339
sptkl <caobaiyue@gmail.com>
2019-12-19 14:50:49 +0000

    removing *.DS_Store, move csv files to the datafolder, remove unused files

--- cbbr_build/05_archive.sh
+++ cbbr_build/05_archive.sh
@@ -9,8 +9,9 @@ docker exec $DB_CONTAINER_NAME bash -c '
         psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS cbbr_submissions;";
         psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.cbbr_submissions;";
         psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.\"$DATE\";";
+        psql $EDM_DATA -c "DROP INDEX IF EXISTS ix_cbbr_submissions_index;";
         psql $EDM_DATA -c "ALTER TABLE cbbr_submissions SET SCHEMA cbbr_submissions;";
         psql $EDM_DATA -c "ALTER TABLE cbbr_submissions.cbbr_submissions RENAME TO \"$DATE\";";
-        psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.latest;";
-        psql $EDM_DATA -c "SELECT * INTO cbbr_submissions.latest FROM cbbr_submissions.\"$DATE\";";
+        psql $EDM_DATA -c "DROP VIEW IF EXISTS cbbr_submissions.latest;";
+        psql $EDM_DATA -c "CREATE VIEW cbbr_submissions.latest AS (SELECT * FROM cbbr_submissions.\"$DATE\");";
     '

ef3bdf0571f58ed12146417de979d155b30654aa
sptkl <caobaiyue@gmail.com>
2019-12-19 22:18:14 +0000

    futurizing ...

--- cbbr_build/05_archive.sh
+++ cbbr_build/05_archive.sh
@@ -3,14 +3,12 @@
 source config.sh
 
 ## to be finished
-docker exec $DB_CONTAINER_NAME bash -c '
-        pg_dump -t cbbr_submissions --no-owner -U postgres -d postgres | psql $EDM_DATA
-        DATE=$(date "+%Y/%m/%d");
-        psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS cbbr_submissions;";
-        psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.cbbr_submissions;";
-        psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.\"$DATE\";";
-        psql $EDM_DATA -c "ALTER TABLE cbbr_submissions SET SCHEMA cbbr_submissions;";
-        psql $EDM_DATA -c "ALTER TABLE cbbr_submissions.cbbr_submissions RENAME TO \"$DATE\";";
-        psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.latest;";
-        psql $EDM_DATA -c "SELECT * INTO cbbr_submissions.latest FROM cbbr_submissions.\"$DATE\";";
-    '
+pg_dump -t cbbr_submissions --no-owner $BUILD_ENGINE | psql $EDM_DATA
+DATE=$(date "+%Y/%m/%d");
+psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS cbbr_submissions;";
+psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.cbbr_submissions;";
+psql $EDM_DATA -c "DROP TABLE IF EXISTS cbbr_submissions.\"$DATE\";";
+psql $EDM_DATA -c "ALTER TABLE cbbr_submissions SET SCHEMA cbbr_submissions;";
+psql $EDM_DATA -c "ALTER TABLE cbbr_submissions.cbbr_submissions RENAME TO \"$DATE\";";
+psql $EDM_DATA -c "DROP VIEW IF EXISTS cbbr_submissions.latest;";
+psql $EDM_DATA -c "CREATE VIEW cbbr_submissions.latest AS (SELECT * FROM cbbr_submissions.\"$DATE\");"

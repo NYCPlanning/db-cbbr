edc790a163ad1823bd701902ad6c60d337aa3cc4
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-12-30 13:16:16 -0500

    commenting out sql query because we don't mneed to run it

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source config.sh
 
-psql $BUILD_ENGINE -f sql/cbbr_submissions.sql
+# psql $BUILD_ENGINE -f sql/cbbr_submissions.sql
 psql $BUILD_ENGINE -f sql/normalize_agency.sql
 psql $BUILD_ENGINE -f sql/normalize_commdist.sql
 psql $BUILD_ENGINE -f sql/normalize_denominator.sql

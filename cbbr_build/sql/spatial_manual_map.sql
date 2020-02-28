4984e79169b335de327db4448131ae068e3136b8
Baoling Zhou <baolingz@nyu.edu>
2020-02-28 16:57:54 +0000

    update geo_function assignment

--- cbbr_build/sql/spatial_manual_map.sql
+++ cbbr_build/sql/spatial_manual_map.sql
@@ -1,6 +1,6 @@
 -- Overwrite geoms with manual mapping input
 UPDATE cbbr_submissions a
 SET geom = b.geom,
-	geo_function = 'Manual_Mapping'
+	geo_function = 'Manual_Research'
 FROM manual_geoms."FY21" b
 WHERE a.unique_id = b.unique_id;

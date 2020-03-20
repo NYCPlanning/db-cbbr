dbaea95d5f455d5cefc3a33293e9fb79909f23fd
mgraber <molly.graber@me.com>
2020-03-20 16:09:30 +0000

    Include missing agencies from normalization

--- cbbr_build/sql/apply_agency_updates.sql
+++ cbbr_build/sql/apply_agency_updates.sql
@@ -1,6 +1,13 @@
 -- Apply updates to agency and acronym
 UPDATE cbbr_submissions 
 SET agency = a.agency,
-    agencyacro = a.agencyacro
+    agency_acronym = upper(a.agencyacro)
 FROM cbbr_agency_updates a 
 WHERE cbbr_submissions.tracking_code = a.trkno
+AND a.agency IS NOT NULL
+AND a.agencyacro IS NOT NULL;
+
+-- Fix change to Queens Public Library
+UPDATE cbbr_submissions 
+SET agency_acronym = 'QPL'
+WHERE cbbr_submissions.agency_acronym = 'QL';

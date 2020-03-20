7455c59c0441839002f0872c74a245fb33636e1d
mgraber <molly.graber@me.com>
2020-03-20 15:44:01 +0000

    Include agency updates in build script

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -2,6 +2,7 @@
 source config.sh
 
 # psql $BUILD_ENGINE -f sql/cbbr_submissions.sql
+psql $BUILD_ENGINE -f sql/apply_agency_updates.sql
 psql $BUILD_ENGINE -f sql/normalize_agency.sql
 psql $BUILD_ENGINE -f sql/normalize_commdist.sql
 psql $BUILD_ENGINE -f sql/normalize_denominator.sql

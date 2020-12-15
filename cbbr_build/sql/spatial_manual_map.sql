766ffc09c806af008a6e81f7f13d23ed8b5f3cff
mgraber <molly.graber@me.com>
2020-12-15 13:56:30 +0000

    Add FY22 manual geoms

--- cbbr_build/sql/spatial_manual_map.sql
+++ cbbr_build/sql/spatial_manual_map.sql
@@ -4,3 +4,9 @@ SET geom = b.geom,
 	geo_function = 'Manual_Research'
 FROM manual_geoms."FY21" b
 WHERE a.unique_id = b.unique_id;
+
+UPDATE cbbr_submissions a
+SET geom = b.geom,
+	geo_function = 'Manual_Research'
+FROM manual_geoms."FY22" b
+WHERE a.unique_id = b.unique_id;

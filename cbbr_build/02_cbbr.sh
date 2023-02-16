a3be6d771b7038e1e482b92670b3bf26e545d9f3
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-16 13:45:07 -0500

    align with previous release commdist values

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 source config.sh
 
+## Skipping for dev of initial FY2024 build
 # psql $BUILD_ENGINE -f sql/cbbr_submissions.sql
+## Skipping for dev of initial FY2024 build
 # psql $BUILD_ENGINE -f sql/apply_agency_updates.sql
+
 echo "Normalizing agency values ..."
-# psql $BUILD_ENGINE -f sql/normalize_agency.sql
 run_sql sql/normalize_agency.sql
 echo "Normalizing commdist values ..."
-# psql $BUILD_ENGINE -f sql/normalize_commdist.sql
 run_sql sql/normalize_commdist.sql
 echo "Normalizing denominator values ..."
-# psql $BUILD_ENGINE -f sql/normalize_denominator.sql
+run_sql sql/normalize_denominator.sql
--- cbbr_build/sql/normalize_commdist.sql
+++ cbbr_build/sql/normalize_commdist.sql
@@ -4,43 +4,49 @@ ALTER TABLE cbbr_submissions
 ALTER TABLE cbbr_submissions
     ADD COLUMN commdist text;
 
+-- OPTION A: Use provided FY2024 values
+-- UPDATE
+--     cbbr_submissions
+-- SET
+--     commdist = boro_and_board;
+-- OPTION B: Construct values to align with previous CBBR versions
 UPDATE
     cbbr_submissions a
 SET
-    commdist = '1' || lpad(cd, 2, '0')
+    commdist = '1' || board
 WHERE
-    a.borough = 'Manhattan'
+    a.boro_name = 'Manhattan'
     AND commdist IS NULL;
 
 UPDATE
     cbbr_submissions a
 SET
-    commdist = '2' || lpad(cd, 2, '0')
+    commdist = '2' || board
 WHERE
-    a.borough = 'Bronx'
+    a.boro_name = 'Bronx'
     AND commdist IS NULL;
 
 UPDATE
     cbbr_submissions a
 SET
-    commdist = '3' || lpad(cd, 2, '0')
+    commdist = '3' || board
 WHERE
-    a.borough = 'Brooklyn'
+    a.boro_name = 'Brooklyn'
     AND commdist IS NULL;
 
 UPDATE
     cbbr_submissions a
 SET
-    commdist = '4' || lpad(cd, 2, '0')
+    commdist = '4' || board
 WHERE
-    a.borough = 'Queens'
+    a.boro_name = 'Queens'
     AND commdist IS NULL;
 
 UPDATE
     cbbr_submissions a
 SET
-    commdist = '5' || lpad(cd, 2, '0')
+    commdist = '5' || board
 WHERE
-    a.borough = 'Staten Island'
+    a.boro_name = 'Staten Island'
     AND commdist IS NULL;
 

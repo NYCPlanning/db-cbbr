862ed87f9a658a0904029a92c2e8859ad9e445eb
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-16 13:07:32 -0500

    start 02 cbbr

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source config.sh
 
+echo "CBBR Version FY2024 : 02 CBBR"
+
 # psql $BUILD_ENGINE -f sql/cbbr_submissions.sql
-psql $BUILD_ENGINE -f sql/apply_agency_updates.sql
+# psql $BUILD_ENGINE -f sql/apply_agency_updates.sql
+echo "Normalizing agency values ..."
 psql $BUILD_ENGINE -f sql/normalize_agency.sql
+echo "Normalizing commdist values ..."
 psql $BUILD_ENGINE -f sql/normalize_commdist.sql
+echo "Normalizing denominator values ..."
 psql $BUILD_ENGINE -f sql/normalize_denominator.sql

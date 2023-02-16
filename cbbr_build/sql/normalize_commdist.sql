29fec050b1268d8cc61a08d3761c4726133ddb40
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-16 13:19:11 -0500

    format

--- cbbr_build/sql/normalize_commdist.sql
+++ cbbr_build/sql/normalize_commdist.sql
@@ -1,29 +1,46 @@
 ALTER TABLE cbbr_submissions
-DROP COLUMN IF EXISTS commdist;
+    DROP COLUMN IF EXISTS commdist;
+
 ALTER TABLE cbbr_submissions
-ADD COLUMN commdist text;
+    ADD COLUMN commdist text;
 
-UPDATE cbbr_submissions a
-SET commdist = '1'||lpad(cd, 2, '0')
-WHERE a.borough='Manhattan'
-AND commdist IS NULL;
+UPDATE
+    cbbr_submissions a
+SET
+    commdist = '1' || lpad(cd, 2, '0')
+WHERE
+    a.borough = 'Manhattan'
+    AND commdist IS NULL;
 
-UPDATE cbbr_submissions a
-SET commdist = '2'||lpad(cd, 2, '0')
-WHERE a.borough='Bronx'
-AND commdist IS NULL;
+UPDATE
+    cbbr_submissions a
+SET
+    commdist = '2' || lpad(cd, 2, '0')
+WHERE
+    a.borough = 'Bronx'
+    AND commdist IS NULL;
 
-UPDATE cbbr_submissions a
-SET commdist = '3'||lpad(cd, 2, '0')
-WHERE a.borough='Brooklyn'
-AND commdist IS NULL;
+UPDATE
+    cbbr_submissions a
+SET
+    commdist = '3' || lpad(cd, 2, '0')
+WHERE
+    a.borough = 'Brooklyn'
+    AND commdist IS NULL;
 
-UPDATE cbbr_submissions a
-SET commdist = '4'||lpad(cd, 2, '0')
-WHERE a.borough='Queens'
-AND commdist IS NULL;
+UPDATE
+    cbbr_submissions a
+SET
+    commdist = '4' || lpad(cd, 2, '0')
+WHERE
+    a.borough = 'Queens'
+    AND commdist IS NULL;
 
-UPDATE cbbr_submissions a
-SET commdist = '5'||lpad(cd, 2, '0')
-WHERE a.borough='Staten Island'
-AND commdist IS NULL;
+UPDATE
+    cbbr_submissions a
+SET
+    commdist = '5' || lpad(cd, 2, '0')
+WHERE
+    a.borough = 'Staten Island'
+    AND commdist IS NULL;
+

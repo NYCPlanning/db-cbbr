78d6c970988f12444c6075006065f70fe7b67d55
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-17 15:58:25 -0500

    rename columns for export

--- cbbr_build/sql/preprocessing.sql
+++ cbbr_build/sql/preprocessing.sql
@@ -32,6 +32,13 @@ UPDATE
 SET
     unique_id = ogc_fid;
 
+-- create columns expected in export
+ALTER TABLE _cbbr_submissions
+    ADD need text;
+
+ALTER TABLE _cbbr_submissions
+    ADD parent_tracking_code text;
+
 -- rename columns
 ALTER TABLE _cbbr_submissions RENAME COLUMN trkno TO tracking_code;
 
@@ -40,6 +47,18 @@ ALTER TABLE _cbbr_submissions RENAME COLUMN boro TO borough;
 ALTER TABLE _cbbr_submissions RENAME COLUMN board TO cd;
 
 ALTER TABLE _cbbr_submissions RENAME COLUMN agency_name TO agency_acronym;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN 'type' TO type_br;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN reason TO explanation;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN supported_by_1 TO supporters1;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN supported_by_2 TO supporters1;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN agy_response_category TO agency_category_response;
+
+ALTER TABLE _cbbr_submissions RENAME COLUMN agy_response TO agency_response;
 
 ALTER TABLE _doitt_buildingfootprints RENAME COLUMN wkb_geometry TO geom;
 

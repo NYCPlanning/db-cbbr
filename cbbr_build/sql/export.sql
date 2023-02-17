3f93e381c0991578a67bae1600473ceff3af0c7e
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-17 15:37:53 -0500

    export modified table not input

--- cbbr_build/sql/export.sql
+++ cbbr_build/sql/export.sql
@@ -1,5 +1,6 @@
 -- Reorder and rename columns to export
 DROP TABLE IF EXISTS cbbr_export;
+
 CREATE TABLE cbbr_export (
     trackingnum text,
     borough text,
@@ -12,7 +13,7 @@ CREATE TABLE cbbr_export (
     need text,
     request text,
     explanation text,
-    sitename text, 
+    sitename text,
     address text,
     streetsegment text,
     streetcross1 text,
@@ -23,60 +24,46 @@ CREATE TABLE cbbr_export (
     agyresponsecat text,
     agyresponse text,
     unique_id text,
-    geom geometry(Geometry,4326));
+    geom geometry(Geometry, 4326)
+);
 
-INSERT INTO cbbr_export(
-    trackingnum,
+INSERT INTO cbbr_export (trackingnum, borough, cd, commdist, type_br, priority, agencyacro, agency, need, request, explanation, sitename, address, streetsegment, streetcross1, streetcross2, supporters1, supporters2, parent_tracking_code, agyresponsecat, agyresponse, unique_id, geom)
+SELECT
+    tracking_code,
     borough,
     cd,
     commdist,
     type_br,
     priority,
-    agencyacro,
+    agency_acronym,
     agency,
     need,
     request,
     explanation,
-    sitename,
-    address,
-    streetsegment,
-    streetcross1,
-    streetcross2,
-    supporters1,
-    supporters2,
-    parent_tracking_code,
-    agyresponsecat,
-    agyresponse,
-    unique_id,
-    geom
-)
-
-SELECT  
-    tracking_code,
-    borough,	
-    cd,
-    commdist,	
-    type_br,		
-    priority,	
-    agency_acronym,
-    agency,
-    need,	
-    request,
-    explanation,
     facility_or_park_name,
     address,
-    street_name,	
-    between_cross_street_1,	
+    street_name,
+    between_cross_street_1,
     and_cross_street_2,
-    (CASE WHEN supporters_1 IS NULL
-			OR supporters_1 IN ('', ' ', 'n/a') THEN NULL
-	  ELSE supporters_1 END) as supporters1,
-    (CASE WHEN supporters_2 IS NULL
-			OR supporters_2 IN ('', ' ', 'n/a') THEN NULL
-	  ELSE supporters_2 END) as supporters2,
+    (
+        CASE WHEN supporters_1 IS NULL
+            OR supporters_1 IN ('', ' ', 'n/a') THEN
+            NULL
+        ELSE
+            supporters_1
+        END) AS supporters1,
+    (
+        CASE WHEN supporters_2 IS NULL
+            OR supporters_2 IN ('', ' ', 'n/a') THEN
+            NULL
+        ELSE
+            supporters_2
+        END) AS supporters2,
     parent_tracking_code,
     agency_category_response,
     agency_response,
     unique_id,
     geom
-FROM cbbr_submissions;
+FROM
+    _cbbr_submissions;
+

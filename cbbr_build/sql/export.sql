81a930ee4c2bfe65a3cf6a721254eeafb2b16324
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-18 13:18:39 -0500

    better exporting

--- cbbr_build/sql/export.sql
+++ cbbr_build/sql/export.sql
@@ -1,50 +1,34 @@
--- Reorder and rename columns to export
+-- Create tables to export and reorder and rename columns
+-- cbbr_submissions_needgeoms
+DROP TABLE IF EXISTS cbbr_submissions_needgeoms;
+
+SELECT
+    * INTO cbbr_submissions_needgeoms
+FROM
+    _cbbr_submissions
+WHERE
+    geom IS NULL;
+
+-- cbbr_export
 DROP TABLE IF EXISTS cbbr_export;
 
-CREATE TABLE cbbr_export (
-    trackingnum text,
-    borough text,
-    cd text,
-    commdist text,
-    type_br text,
-    priority text,
-    agencyacro text,
-    agency text,
-    need text,
-    request text,
-    explanation text,
-    sitename text,
-    address text,
-    streetsegment text,
-    streetcross1 text,
-    streetcross2 text,
-    supporters1 text,
-    supporters2 text,
-    parent_tracking_code text,
-    agyresponsecat text,
-    agyresponse text,
-    unique_id text,
-    geom geometry(Geometry, 4326)
-);
-
-INSERT INTO cbbr_export (trackingnum, borough, cd, commdist, type_br, priority, agencyacro, agency, need, request, explanation, sitename, address, streetsegment, streetcross1, streetcross2, supporters1, supporters2, parent_tracking_code, agyresponsecat, agyresponse, unique_id, geom)
 SELECT
-    tracking_code,
+    tracking_code AS trackingnum,
     borough,
     cd,
     commdist,
     type_br,
     priority,
-    agency_acronym,
+    agency_acronym AS agencyacro,
     agency,
     need,
     request,
     explanation,
-    facility_or_park_name,
+    facility_or_park_name AS sitename,
     address,
-    street_name,
-    between_cross_street_1,
-    and_cross_street_2,
+    street_name AS streetsegment,
+    between_cross_street_1 AS streetcross1,
+    and_cross_street_2 AS streetcross2,
     (
         CASE WHEN supporters_1 IS NULL
             OR supporters_1 IN ('', ' ', 'n/a') THEN
@@ -60,10 +44,35 @@ SELECT
             supporters_2
         END) AS supporters2,
     parent_tracking_code,
-    agency_category_response,
-    agency_response,
+    agency_category_response AS agyresponsecat,
+    agency_response AS agyresponse,
     unique_id,
-    geom
+    geo_function,
+    geom INTO cbbr_export
 FROM
     _cbbr_submissions;
+
+-- cbbr_export_poly
+DROP TABLE IF EXISTS cbbr_export_poly;
+
+SELECT
+    * INTO cbbr_export_poly
+FROM
+    cbbr_export
+WHERE
+    ST_GeometryType (geom) = 'ST_MultiPolygon';
+
+-- cbbr_export_pts
+DROP TABLE IF EXISTS cbbr_export_pts;
+
+SELECT
+    * INTO cbbr_export_pts
+FROM
+    cbbr_export
+WHERE
+    ST_GeometryType (geom) = 'ST_MultiPoint';
+
+-- drop geom column from cbbr_export
+ALTER TABLE cbbr_export
+    DROP COLUMN IF EXISTS geom;
 

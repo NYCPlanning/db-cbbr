1d20f9ded6691c681902d50ad2a5b62e22c36208
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-21 10:33:06 -0500

    rename export files

--- cbbr_build/04_export.sh
+++ cbbr_build/04_export.sh
@@ -10,15 +10,17 @@ run_sql sql/export.sql
 
 cd $OUTPUT_DIRECTORY
 echo "Exporting input table to csv files ..."
-CSV_export $BUILD_ENGINE cbbr_submissions cbbr_submissions
+CSV_export $BUILD_ENGINE cbbr_submissions cbbr_submissions_input
 
 echo "Exporting output tables to csv files ..."
-CSV_export $BUILD_ENGINE _cbbr_submissions _cbbr_submissions
+CSV_export $BUILD_ENGINE _cbbr_submissions cbbr_submissions_build
 CSV_export $BUILD_ENGINE cbbr_export cbbr_export
 
 CSV_export $BUILD_ENGINE cbbr_submissions_needgeoms cbbr_submissions_needgeoms
 CSV_export $BUILD_ENGINE cbbr_export_poly cbbr_submissions_poly
 CSV_export $BUILD_ENGINE cbbr_export_pts cbbr_submissions_pts
+
+echo "Exporting output tables to shapefile zip files ..."
 SHP_export $BUILD_ENGINE cbbr_export_poly MULTIPOLYGON cbbr_submissions_poly
 SHP_export $BUILD_ENGINE cbbr_export_pts MULTIPOINT cbbr_submissions_pts
 

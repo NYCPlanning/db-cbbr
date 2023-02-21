eca8cdb3eab643191ec69a2ed8b0a8b33ffb5bc0
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-21 10:32:26 -0500

    text change

--- cbbr_build/02_cbbr.sh
+++ cbbr_build/02_cbbr.sh
@@ -2,7 +2,7 @@
 source config.sh
 
 echo "CBBR Version $VERSION : 02 CBBR"
-echo "Create tables to modify ..."
+echo "Create build tables to modify ..."
 run_sql sql/preprocessing.sql
 
 ## Skipping for dev of initial FY2024 build
@@ -10,8 +10,10 @@ run_sql sql/preprocessing.sql
 
 echo "Normalize agency values ..."
 run_sql sql/normalize_agency.sql
+
 echo "Normalize commdist values ..."
 run_sql sql/normalize_commdist.sql
+
 echo "Normalize denominator values ..."
 run_sql sql/normalize_denominator.sql
 

8f1e3fbbc6ff48c9ec760de014d6e645eb6df51c
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-25 23:02:30 -0500

    format

--- cbbr_build/library/helper/engines.py
+++ cbbr_build/library/helper/engines.py
@@ -5,11 +5,12 @@ from urllib.parse import urlparse
 import psycopg2
 import os
 
-load_dotenv(Path(__file__).parent.parent/'.env')
+load_dotenv(Path(__file__).parent.parent / ".env")
 
-recipe_engine = create_engine(os.getenv('RECIPE_ENGINE'))
-edm_engine = create_engine(os.getenv('EDM_DATA'))
-build_engine = create_engine(os.getenv('BUILD_ENGINE'))
+recipe_engine = create_engine(os.getenv("RECIPE_ENGINE"))
+edm_engine = create_engine(os.getenv("EDM_DATA"))
+build_engine = create_engine(os.getenv("BUILD_ENGINE"))
+
 
 def psycopg2_connect(url):
     result = urlparse(str(url))
@@ -19,9 +20,6 @@ def psycopg2_connect(url):
     hostname = result.hostname
     port = result.port
     connection = psycopg2.connect(
-        database = database,
-        user = username,
-        password = password,
-        host = hostname, 
-        port = port)
+        database=database, user=username, password=password, host=hostname, port=port
+    )
     return connection
